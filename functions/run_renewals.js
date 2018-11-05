98292dae788e6b8fe9e0ce6e964ca99207a8146d
Biko Allen <biko.allen@gmail.com>
2018-11-05 15:18:14 -0600

    Update renewal function

--- functions/run_renewals.js
+++ functions/run_renewals.js
@@ -1,4 +1,4 @@
-'use strict'
+/*'use strict'
 var mongoose = require('mongoose')
 var co = require('co')
 
@@ -31,8 +31,8 @@ exports.handler = function(event, context, callback) {
     .then(() => {
       callback(null, response)
     })
-    .catch(onerror)
-  /*
+    .catch(onerror)*/
+/*
   co(function*() {
     if (conn == null) {
       conn = yield mongoose.createConnection(uri, {
@@ -49,7 +49,7 @@ exports.handler = function(event, context, callback) {
     })
   }).catch(onerror)
 */
-  /*
+/*
   conn = mongoose
     .createConnection(uri, {
       bufferCommands: false,
@@ -69,7 +69,7 @@ exports.handler = function(event, context, callback) {
         body: 'MongoDB: Connection failed!',
       })
     )*/
-  /*
+/*
     const M = conn.model(
       'Customer',
       new mongoose.Schema({
@@ -92,11 +92,70 @@ exports.handler = function(event, context, callback) {
     //console.log(uri)
     return response
     */
-}
-
+//}
+/*
 function onerror(err) {
   // log any uncaught errors
   // co will not throw any errors you do not handle!!!
   // HANDLE ALL YOUR ERRORS!!!
   console.error(err.stack)
+}
+*/
+const co = require('co')
+const mongoose = require('mongoose')
+
+let conn = null
+
+const { DBUSER, DBPASS, DBPATH } = process.env
+
+const uri = 'mongodb://' + DBUSER + ':' + DBPASS + '@' + DBPATH
+
+exports.handler = function(event, context, callback) {
+  // Make sure to add this so you can re-use `conn` between function calls.
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+  context.callbackWaitsForEmptyEventLoop = false
+
+  run()
+    .then(res => {
+      callback(null, res)
+      console.log('done')
+    })
+    .catch(error => callback(error))
+}
+
+function run() {
+  return co(function*() {
+    // Because `conn` is in the global scope, Lambda may retain it between
+    // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+    // This means your Lambda function doesn't have to go through the
+    // potentially expensive process of connecting to MongoDB every time.
+    if (conn == null) {
+      conn = yield mongoose.createConnection(uri, {
+        // Buffering means mongoose will queue up operations if it gets
+        // disconnected from MongoDB and send them when it reconnects.
+        // With serverless, better to fail fast if not connected.
+        bufferCommands: false, // Disable mongoose buffering
+        bufferMaxEntries: 0, // and MongoDB driver buffering
+      })
+      conn.model(
+        'Customer',
+        new mongoose.Schema({
+          firstName: String,
+          lastName: String,
+          phone: String,
+          payDay: String,
+          amount: String,
+          notes: String,
+          updated_At: { type: Date, default: Date.now },
+        })
+      )
+    }
+
+    const M = conn.model('Customer')
+
+    const doc = yield M.findOne()
+    console.log(doc)
+
+    return doc
+  })
 }
