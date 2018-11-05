12eeb1d427aae86c330ca9741961b36644b420d6
Biko Allen <biko.allen@gmail.com>
2018-11-05 15:21:01 -0600

    Update renewal function

--- functions/run_renewals.js
+++ functions/run_renewals.js
@@ -1,106 +1,3 @@
-/*'use strict'
-var mongoose = require('mongoose')
-var co = require('co')
-
-let conn = null
-
-const { DBUSER, DBPASS, DBPATH } = process.env
-
-const uri = 'mongodb://' + DBUSER + ':' + DBPASS + '@' + DBPATH
-
-exports.handler = function(event, context, callback) {
-  //context.callbackWaitForEmptyEventLoop = false
-
-  let response
-
-  return mongoose
-    .connect(
-      uri,
-      {
-        bufferCommands: false,
-        bufferMaxEntries: 0,
-        useNewUrlParser: true,
-      }
-    )
-    .then(ee => {
-      response = { hello: 'world' }
-    })
-    .then(() => {
-      mongoose.disconnect()
-    })
-    .then(() => {
-      callback(null, response)
-    })
-    .catch(onerror)*/
-/*
-  co(function*() {
-    if (conn == null) {
-      conn = yield mongoose.createConnection(uri, {
-        bufferCommands: false,
-        bufferMaxEntries: 0,
-        useNewUrlParser: true,
-        useMongoClient: false,
-      })
-    }
-    console.log('Success!')
-    callback(null, {
-      statusCode: 200,
-      body: 'Success!',
-    })
-  }).catch(onerror)
-*/
-/*
-  conn = mongoose
-    .createConnection(uri, {
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-      useNewUrlParser: true,
-    })
-    .then(() => {
-      console.log('MongoDB: Connection successful!')
-      callback(null, {
-        statusCode: 200,
-        body: 'Success!',
-      })
-    })
-    .catch(error =>
-      callback(null, {
-        statusCode: 400,
-        body: 'MongoDB: Connection failed!',
-      })
-    )*/
-/*
-    const M = conn.model(
-      'Customer',
-      new mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        phone: String,
-        payDay: String,
-        amount: String,
-        notes: String,
-        updated_At: { type: Date, default: Date.now },
-      })
-    )
-
-    const doc = M.find()
-    //console.log(doc)
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(doc),
-    }
-    //console.log(uri)
-    return response
-    */
-//}
-/*
-function onerror(err) {
-  // log any uncaught errors
-  // co will not throw any errors you do not handle!!!
-  // HANDLE ALL YOUR ERRORS!!!
-  console.error(err.stack)
-}
-*/
 const co = require('co')
 const mongoose = require('mongoose')
 
@@ -136,6 +33,7 @@ function run() {
         // With serverless, better to fail fast if not connected.
         bufferCommands: false, // Disable mongoose buffering
         bufferMaxEntries: 0, // and MongoDB driver buffering
+        useNewUrlParser: true, // current URL string parser is deprecated
       })
       conn.model(
         'Customer',
