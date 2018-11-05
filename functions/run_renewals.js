98d26d5b423d5e74f92b4d26390598e49805f6e6
Biko Allen <biko.allen@gmail.com>
2018-11-05 09:10:15 -0600

    Update renewal function

--- functions/run_renewals.js
+++ functions/run_renewals.js
@@ -12,43 +12,52 @@ const uri =
   '@ds151753.mlab.com:51753/renewalreminder'
 
 exports.handler = function(event, context, callback) {
-  conn = mongoose
-    .createConnection(uri, {
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-      useNewUrlParser: true,
-    })
-    .then(
-      () => {
-        console.log('MongoDB: Connection successful!')
-        return 'Connection successful!'
-      },
-      err => {
-        console.error('MongoDB: ' + err)
-        return err
-      }
-    )
-  /*
-  const M = conn.model(
-    'Customer',
-    new mongoose.Schema({
-      firstName: String,
-      lastName: String,
-      phone: String,
-      payDay: String,
-      amount: String,
-      notes: String,
-      updated_At: { type: Date, default: Date.now },
-    })
-  )
+  context.callbackWaitForEmptyEventLoop = false
 
-  const doc = M.find()
-  //console.log(doc)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(doc),
+  run()
+    .then(res => {
+      callback(null, res)
+    })
+    .catch(error => callback(error))
+
+  function run() {
+    conn = mongoose
+      .createConnection(uri, {
+        bufferCommands: false,
+        bufferMaxEntries: 0,
+        useNewUrlParser: true,
+      })
+      .then(
+        () => {
+          console.log('MongoDB: Connection successful!')
+          return 'Success!'
+        },
+        err => {
+          console.error('MongoDB: ' + err)
+          return err
+        }
+      )
+
+    const M = conn.model(
+      'Customer',
+      new mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        phone: String,
+        payDay: String,
+        amount: String,
+        notes: String,
+        updated_At: { type: Date, default: Date.now },
+      })
+    )
+
+    const doc = M.find()
+    //console.log(doc)
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(doc),
+    }
+    //console.log(uri)
+    return response
   }
-  //console.log(uri)
-  return response
-  */
 }
